3ade69bfb3957af6438e84de2e74c38630295296
Dmitry Monakhov <dimamon6032403@gmail.com>
2020-09-20 18:51:20 +0300

    Initial commit

---
+++ FrequencySampler/FrequencySampler/config.h
@@ -0,0 +1,19 @@
+/*
+ * config.h
+ *
+ * Created: 20.09.2020 18:47:16
+ *  Author: DMITRYBIGPC
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <avr/io.h>
+
+#define TEMP_SENSOR_PORT	PORTB
+#define TEMP_SENSOR_PIN		
+
+
+
+#endif /* CONFIG_H_ */
---
+++ FrequencySampler/FrequencySampler/main.c
@@ -0,0 +1,18 @@
+/*
+ * FrequencySampler.c
+ *
+ * Created: 20.09.2020 18:45:48
+ * Author : DMITRYBIGPC
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
